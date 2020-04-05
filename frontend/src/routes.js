b249776582114838b3341ea1843e4e683ec83edf
Daniel Cunha Stock <danielcstock12@gmail.com>
2020-04-05 20:23:27 -0300

    Front-end web finalizado.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes(){
     return (
@@ -9,6 +11,8 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Logon}></Route>
                 <Route path="/register" component={Register}></Route>
+                <Route path="/profile" component={Profile}></Route>
+                <Route path="/incidents/new" component={NewIncident}></Route>
             </Switch>
         </BrowserRouter>
     )
